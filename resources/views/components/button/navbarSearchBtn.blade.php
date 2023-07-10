56d45e1f8a9edc01bb6bc7c5db78a6d23ba2db59
josemaxeichenberger <jme.jose.max@gmail.com>
2023-07-09 22:35:24 -0300

    u

---
+++ resources/views/components/button/navbarSearchBtn.blade.php
@@ -0,0 +1,6 @@
+<button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch"
+    aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
+    <svg class="bi">
+        <use xlink:href="#search" />
+    </svg>
+</button>
---
+++ resources/views/components/button/sidebarMenuBtn.blade.php
@@ -0,0 +1,6 @@
+<button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu"
+    aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <svg class="bi">
+        <use xlink:href="#list" />
+    </svg>
+</button>
